0ab62800ed6acfe4b6dcf133199ed73b22bbe639
sparkus777 <sparkus342@gmail.com>
2022-10-04 22:38:36 +0300

    crawler is rdy

--- crawler.php
+++ crawler.php
@@ -19,7 +19,7 @@ function run(array $arguments): void
     if (isset($arguments[1]) && filter_var($arguments[1], FILTER_VALIDATE_URL)) {
         $url = $arguments[1];
     } else {
-        print('Please enter a valid url.' . PHP_EOL);
+        print('Incorrect url.' . PHP_EOL);
 
         die();
     }
@@ -27,16 +27,29 @@ function run(array $arguments): void
     $urlParts = parse_url($url);
 
     $result = [];
-    $result = crawler($url, $urlParts['host'] ?? '', $result);
+    crawler($url, $urlParts['host'] ?? '', $result);
+
+    uasort($result, function ($first, $second) {
+        return $first <=> $second;
+    });
 
     $endTime = microtime(true) - $start;
+    makeReport($result, $endTime);
 
     print('Success.'. PHP_EOL);
     print("Total time: $endTime" . PHP_EOL);
 }
 
-function crawler(string $url, string $host, array &$result)
+/**
+ * @param string $url
+ * @param string $host
+ * @param array $result
+ * @return array
+ */
+function crawler(string $url, string $host, array &$result): array
 {
+     $startTime = microtime(true);
+
     $headers = [
         'ssl' => [
             "allow_self_signed" => true,
@@ -51,26 +64,39 @@ function crawler(string $url, string $host, array &$result)
 
         [$countImages, $links] = parser($pageData, $dom);
 
-        $result[$url] = $countImages;
+        $result[$url]['count_images'] = $countImages;
 
         foreach ($links as $link) {
             $hrefLink = $link->getAttribute('href');
             $urlParts = parse_url($hrefLink);
 
-            if (filter_var($hrefLink, FILTER_VALIDATE_URL) && isset($urlParts['host']) ? $urlParts['host'] : '' === $host && array_key_exists($hrefLink, $result) === false) {
+            if (array_key_exists($hrefLink, $result)) {
+                continue;
+            }
+
+            if (filter_var($hrefLink, FILTER_VALIDATE_URL) && ($urlParts['host'] ?? '') === $host) {
                 print("Parse ulr in eac. $hrefLink" . PHP_EOL);
+
                 $html = @file_get_contents($hrefLink, false, stream_context_create($headers));
                 if ($html) {
-                    [$countImages] = parser($html, $dom);
-                    $result[$hrefLink] = $countImages;
+                    crawler($hrefLink, $host, $result);
                 }
             }
         }
     }
+
+    $endTime = microtime(true) - $startTime;
+    $result[$url]['total_time'] = $endTime;
+
     return $result;
 }
 
-function parser($html, $dom): array
+/**
+ * @param string $html
+ * @param DOMDocument $dom
+ * @return array
+ */
+function parser(string $html, DOMDocument $dom): array
 {
     @$dom->loadHTML($html);
     $dom->preserveWhiteSpace = false;
@@ -79,4 +105,55 @@ function parser($html, $dom): array
 
     return [$countImages, $links];
 }
+
+/**
+ * @param array $result
+ * @return void
+ */
+function makeReport(array $result, float $totalTime): void
+{
+    $html = '<!DOCTYPE html>
+      <html lang="uk">
+            <head>  
+                <title>Report</title>
+            </head>
+            <style>
+                table, th, tr, td {
+                   border: 1px solid black;
+                   border-collapse: collapse;
+                }
+            </style>
+        <body>
+        <table>
+                <tbody>
+                <tr>
+                        <td>Number of link</td>
+                        <td>Link</td>
+                        <td>Count of images</td>
+                        <td>Total seconds</td>
+                    </tr>
+	            ';
+
+    $i = 1;
+    foreach ($result as $key => $item) {
+        $html .= "<tr>
+                        <td>" . $i++ ."</td>
+                        <td>" . $key. "</td>
+                        <td>" . $item['count_images'] ."</td>
+                        <td>" . round($item['total_time'], 2) ."</td>
+                    </tr>
+	            ";
+    }
+
+    $html .= "<tr>
+                        <td colspan='4'>Total time: $totalTime</td>
+                    </tr>
+                </tbody>
+            </table>
+         </body>
+      </html>";
+
+    file_put_contents('./Reports/report_'. date('Y-m-d_H:i:s') . '.html',  $html);
+}
+
 run($argv);
